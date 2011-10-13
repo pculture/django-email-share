fb1ac165c2c658281b9e5542704e787229987392
Paul Swartz <paulswartz@gmail.com>
2011-10-13 10:56:29 -0400

    actually install some packages

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ version = '0.0.1'
 setup(name="django-email-share",
       version=version,
       author='Participatory Culture Foundation',
-      license='BSD')
+      license='BSD',
+      packages=find_packages())
 
 
